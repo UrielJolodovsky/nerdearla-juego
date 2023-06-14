0c218db8bca3e846624d3f204a99f4d71d742219
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-06-14 14:19:58 -0300

    minor changes

--- nerdearla-game/src/pages/api/index.js
+++ nerdearla-game/src/pages/api/index.js
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import express from 'express'
 import { GetChallenges } from './controller/Challenges.js'
+import cors from 'cors'
 
 const hostname = "127.0.0.1"
 const port = 9000
@@ -9,6 +10,7 @@ import bodyParser from 'body-parser'
 
 app.use(express.json())
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/challenge', GetChallenges) 
 
